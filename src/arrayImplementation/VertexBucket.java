eb86acc2308bc4b89bafc3094d3c37770b97fb4a
Kyle Housley <kyle@bill4time.com>
2016-05-31 18:26:47 -0700

    Adding christian's changes

--- src/arrayImplementation/VertexBucket.java
+++ src/arrayImplementation/VertexBucket.java
@@ -1,12 +1,15 @@
 package arrayImplementation;
 
-import startercode.Vertex;
-
-import java.util.LinkedList;
+import java.util.LinkedList; 
 import java.util.List;
 
-public class VertexBucket {
+import dijkstra.AbstractVertexQueue;
+import dijkstra.VertexData;
+import startercode.Vertex;
 
+public class VertexBucket extends AbstractVertexQueue{
+
+	private List<Vertex>[] listArray;
 	private List<Vertex> vertices = new LinkedList<Vertex>();
 	
 	public void addVertex(Vertex theVertex) {
@@ -20,6 +23,29 @@ public class VertexBucket {
 	public boolean contains(Vertex theVertex) {
 		if (vertices.contains(theVertex)) return true;
 		return false;
+	}
+
+	@Override
+	public void decrease(Vertex v, VertexData newData, int dist) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vertex deleteMin() {
+		return vertices.remove(0);
+	}
+
+	@Override
+	public int insert(Vertex v, int vDist) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getVertexCount() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 }
